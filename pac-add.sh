2388a02c16878a85dcd2cf42d54ca520cbc55d4c
leo starless <33985949+leo1359531@users.noreply.github.com>
2022-04-27 14:08:32 +0200

    Create pac-add.sh

---
+++ pac-add.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel neofetch neovim kitty zsh firefox mako waybar ranger xorg-xwaybar curl wget python-pip fish wl-clipboard libnotify fuse
