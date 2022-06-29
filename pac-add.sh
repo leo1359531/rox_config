abb38da1d7bb846b93ca41131132ab2ecfcab0b1
leo starless <33985949+leo1359531@users.noreply.github.com>
2022-06-29 20:36:03 +0200

    Update pac-add.sh

--- pac-add.sh
+++ pac-add.sh
@@ -3,4 +3,4 @@
 sudo pacman -Syu
 sudo pacman -S --needed git base-devel neofetch neovim kitty zsh firefox mako \
 waybar ranger xorg-xwaybar curl wget python-pip fish wl-clipboard libnotify fuse \
-vi fortune-mod noto-fonts-emoji
+vi fortune-mod noto-fonts-emoji nautilus
